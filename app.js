642d4b7d6211703ba07b4520887c5746a9fda4ea
Maria Andersson <monstermaria@gmail.com>
2019-11-15 11:02:05 +0100

    Created comments to show the structure of the code

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+/* The user choses a city to show information about by using a dropdown
+list and the clicking the search button.
+
+This calls the initial function that reads the input, and calls the 
+other functions with the input as parameter.
+
+The other functions gets data from their respective sources, and 
+presents it in their corresponding divs on the homepage
+*/
+
+
+// Function to read input and call other functions that collect data
+
+
+
+// Function that gets weather data from openweathermap
+
+
+
+// Function that reads JSON data from file
+
+
 
