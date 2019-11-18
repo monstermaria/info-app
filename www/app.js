b7200be59588ea7e8f1154d572bde34a1c153a1e
margareta75 <margareta.xx@gmail.com>
2019-11-18 19:21:40 +0100

    några ändringar i css, html och js

--- www/app.js
+++ www/app.js
@@ -52,7 +52,7 @@ function moreInfo(city) {
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
-            document.getElementById("test").innerHTML = this.responseText;
+            document.getElementById("info").innerHTML = this.responseText;
         }
     };
     xhttp.open("GET", "info.json", true);
